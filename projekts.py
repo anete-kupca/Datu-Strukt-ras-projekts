dc2c6ea7549bff4de0fb1ef79f069239acce13bf
Anete Kupča <anete.kupca@edu.rtu.lv>
2025-05-19 23:07:07 +0300

    izvada majaslapas html

--- projekts.py
+++ projekts.py
@@ -1,10 +1,11 @@
-import requests as rq
+import requests
 from bs4 import BeautifulSoup
 
-darbs = input("Ievadi darba nosaukumu: ")
+#darbs = input("Ievadi darba nosaukumu: ")
 url = "https://cv.lv/lv/search?limit=20&offset=0&categories%5B0%5D=INFORMATION_TECHNOLOGY&fuzzy=true&searchId=dba27038-d36b-4d0a-98c6-b92385b9568b"
 
-lapa = rq.get(url)
+lapa = requests.get(url)
 
 if lapa.status_code == 200:
-    print("Lapa ir pieejama")
+    lapas_saturs = BeautifulSoup(lapa.text, "html.parser")
+    print(lapas_saturs.prettify())
